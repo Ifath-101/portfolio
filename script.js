7d05a87bb33e3a76293d0304ff153f64e3b108af
Ifath-101 <ifathahamed01@gmail.com>
2024-05-29 18:32:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    function mouseover(){
+        document.getElementById("demo").style.fontSize="50px";
+        }
+    function mouseout(){
+        document.getElementById("demo").style.fontSize="40px";
+        }
+</script>
